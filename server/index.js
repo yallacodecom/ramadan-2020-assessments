26bcb60c11548b5694d0601e292c80d16077f13c
abdoulrhman <abdoulrhman_salah@hotmail.com>
2020-04-26 15:16:17 +0200

    Task_One_Post_Video_Request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 7777;
-const VideoRequestData = require('./data/video-requests.data');
-const cors = require('cors');
-const mongoose = require('./models/mongo.config');
+const VideoRequestData = require("./data/video-requests.data");
+const cors = require("cors");
+const mongoose = require("./models/mongo.config");
 
 if (!Object.keys(mongoose).length) return;
 
@@ -12,35 +12,35 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-  res.send('Welcome to semicolon academy APIs, use /video-request to get data')
+app.get("/", (req, res) =>
+  res.send("Welcome to semicolon academy APIs, use /video-request to get data")
 );
 
-app.post('/video-request', async (req, res, next) => {
+app.post("/video-request", async (req, res, next) => {
   const response = await VideoRequestData.createRequest(req.body);
   res.send(response);
   next();
 });
 
-app.get('/video-request', async (req, res, next) => {
+app.get("/video-request", async (req, res, next) => {
   const data = await VideoRequestData.getAllVideoRequests();
   res.send(data);
   next();
 });
 
-app.put('/video-request', async (req, res, next) => {
+app.put("/video-request", async (req, res, next) => {
   const response = await VideoRequestData.updateRequest(req.body.id, req.body);
   res.send(response);
   next();
 });
 
-app.get('/users', async (req, res, next) => {
+app.get("/users", async (req, res, next) => {
   const response = await UserData.getAllUsers(req.body);
   res.send(response);
   next();
 });
 
-app.post('/users/login', async (req, res, next) => {
+app.post("/users/login", async (req, res, next) => {
   const response = await UserData.createUser(req.body);
   res.redirect(`http://localhost:5500?id=${response._id}`);
   next();
@@ -48,14 +48,14 @@ app.post('/users/login', async (req, res, next) => {
 
 app.use(express.json());
 
-app.put('/video-request/vote', async (req, res, next) => {
+app.put("/video-request/vote", async (req, res, next) => {
   const { id, vote_type } = req.body;
   const response = await VideoRequestData.updateVoteForRequest(id, vote_type);
   res.send(response);
   next();
 });
 
-app.delete('/video-request', async (req, res, next) => {
+app.delete("/video-request", async (req, res, next) => {
   const response = await VideoRequestData.deleteRequest(req.body.id);
   res.send(response);
   next();
