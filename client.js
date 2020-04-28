ea7d9a5a615417d3430e14629c418fb92ab6a52f
Said Bakr <said.fox@gmail.com>
2020-04-28 23:04:21 +0200

    Cleaning the code, adding separate js files and adding comments.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   client.js
    	modified:   index.html
    	new file:   template.js
    
        Changes not staged for commit:
    	modified:   server/data/video-requests.data.js
    	modified:   server/models/mongo.config.js
    
     Change the `tpl_inst` object design to be global object defined independently to the `render_tpl()` so `render_tpl()` becomes usable

---
+++ client.js
@@ -0,0 +1,87 @@
+// Template instructions object. The property key, is the variable {{name}} in the template
+// and the value is the response key name.
+var tpl_inst = {
+    title: 'topic_title',
+    details: 'topic_details',
+    expected: 'expected_result',
+    state: 'status',
+    name: 'author_name',
+    email: 'author_email',
+    dated: 'submit_date',
+    level: 'target_level'
+}
+var listOfRequests = document.getElementById('listOfRequests');
+
+/**
+ * 
+ * @param {String} tpl The HTML of the template with its variable in the form {{varName}}
+ * @param {Object} tpl_inst The template instructions or mapping that maps each template variable as a key to its expected response key name
+ * @param {Object} res The response data object that obeyes the instructions in tpl_inst
+ * @returns {String} The rendered HTML template with its variables values.
+ */
+function render_tpl(tpl, tpl_inst, res) {
+    var output = tpl // From /template.js
+    Object.keys(tpl_inst).forEach((k) => {
+        var replace = "{{" + k + "}}";
+        var re = new RegExp(replace, "g");
+        output = output.replace(re, res[tpl_inst[k]])
+    })
+    return output
+}
+
+/**
+ * After XHR success, it insterts each response item to the listOfRequests videos.
+ */
+
+function loadVideos() {
+    if (readVideos.readyState === XMLHttpRequest.DONE) {
+        if (readVideos.status === 200) {
+            var response = JSON.parse(readVideos.responseText);            
+            for (var res of response) {                
+                listOfRequests.insertAdjacentHTML('beforeend', render_tpl(tpl, tpl_inst, res));                
+            }
+        } else {
+            alert('There was a problem with loading videos list!');
+        }
+    }
+}
+
+function checkSaving() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            alert('New video request has been created with an ID:\n' + response._id);
+            formElements = createForm.elements
+            for (i = 0; i < formElements.length; i++) {
+                formElements[i].value = '';
+            }            
+            listOfRequests.insertAdjacentHTML('afterbegin', render_tpl(tpl, tpl_inst, response));
+        } else {
+            alert('There was a problem with the request.');
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    // Getting Videos list.
+    readVideos = new XMLHttpRequest();
+    readVideos.onreadystatechange = loadVideos;
+    readVideos.open("GET", 'http://localhost:7777/video-request', true);    
+    readVideos.send();
+    //
+    // Handling the form, preparing the form's data and submitting the form via XHR
+    const createForm = document.getElementById('createForm');
+    createForm.addEventListener('submit', (e) => {        
+        e.preventDefault()
+        xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = checkSaving;
+        xhr.open("POST", "http://localhost:7777/video-request", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        data = new FormData(createForm);
+        postData = ''
+        for (i of data.entries()) {
+            postData += i[0] + '=' + i[1] + '&'
+        }        
+        xhr.send(postData)
+    });
+    //
+});
---
+++ template.js
@@ -0,0 +1,30 @@
+tpl = `<div class="card mb-3">
+          <div class="card-body d-flex justify-content-between flex-row">
+            <div class="d-flex flex-column">
+              <h3>{{title}}</h3>
+              <p class="text-muted mb-2">{{details}}</p>
+              <p class="mb-0 text-muted">
+                <strong>Expected results:</strong> {{expected}}
+              </p>
+            </div>
+            <div class="d-flex flex-column text-center">
+              <a class="btn btn-link">🔺</a>
+              <h3>0</h3>
+              <a class="btn btn-link">🔻</a>
+            </div>
+          </div>
+          <div class="card-footer d-flex flex-row justify-content-between">
+            <div>
+              <span class="text-info">{{state}}</span>
+              &bullet; added by <strong>{{name}}</strong> on
+              <strong>{{dated}}</strong>
+            </div>
+            <div
+              class="d-flex justify-content-center flex-column 408ml-auto mr-2"
+            >
+              <div class="badge badge-success">
+                {{level}}
+              </div>
+            </div>
+          </div>
+        </div>`;
