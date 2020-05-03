cbeef1084f979e91fcf1690d01941bac7ebf0397
Medhat Dawoud <medhatdawoud@gmail.com>
2020-05-03 12:27:01 +0200

    Episode 7

--- client.js
+++ client.js
@@ -98,6 +98,45 @@ function debounce(fn, time) {
   };
 }
 
+function checkValidity(formData) {
+  const name = formData.get('author_name');
+  const email = formData.get('author_email');
+  const topic = formData.get('topic_title');
+  const topicDetails = formData.get('topic_details');
+
+  if (!name) {
+    document.querySelector('[name=author_name]').classList.add('is-invalid');
+  }
+
+  const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!email || !emailPattern.test(email)) {
+    document.querySelector('[name=author_email]').classList.add('is-invalid');
+  }
+
+  if (!topic || topic.length > 30) {
+    document.querySelector('[name=topic_title]').classList.add('is-invalid');
+  }
+
+  if (!topicDetails) {
+    document.querySelector('[name=topic_details]').classList.add('is-invalid');
+  }
+
+  const allInvalidElms = document
+    .getElementById('formVideoRequest')
+    .querySelectorAll('.is-invalid');
+
+  if (allInvalidElms.length) {
+    allInvalidElms.forEach((elm) => {
+      elm.addEventListener('input', function () {
+        this.classList.remove('is-invalid');
+      });
+    });
+    return false;
+  }
+
+  return true;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   const formVidReqElm = document.getElementById('formVideoRequest');
   const sortByElms = document.querySelectorAll('[id*=sort_by_]');
@@ -136,6 +175,10 @@ document.addEventListener('DOMContentLoaded', function () {
     e.preventDefault();
 
     const formData = new FormData(formVidReqElm);
+
+    const isValid = checkValidity(formData);
+
+    if (!isValid) return;
 
     fetch('http://localhost:7777/video-request', {
       method: 'POST',
